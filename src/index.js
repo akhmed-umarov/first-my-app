f64c65da305a8afcf94bcb8b979cda5e89f1e78f
Akhmed <ahmad.umarov7@gmail.com>
2022-06-13 20:22:19 +0300

    end 2 lesson

--- src/index.js
+++ src/index.js
@@ -2,16 +2,60 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+
+///babel трансформирует react в обычный код 
+//в старом реакте надо импортировать реакт в каждый файл
+
+
+const element = <h2>Hello World!</h2> 
+//одно и то же
+// const element = React.createElement('h2', {className: `great`} , "Hello World!"); 
+
+// из этого метода возвращается объект такого формата
+// const element = { 
+//   type: 'h2', 
+//   props: { 
+//     className: 'great', 
+//     children: 'Hello World!'
+//   }
+// }
+
+
+const text = 'Hello World!'
+
+
+//если несколько тегов то надо элемент указывать в скобках 
+//даже если указывать многострочный объект у него должен быть только один родитель
+///{ } тоже самое что инторполяция ${} нельзя в них засовывать объекты (кроме масива)
+const elem = ( 
+  <div>
+    <h2 className='Текст'> Текст: {text} </h2>
+    <input type="text" />
+    <label htmlFor="text"></label>
+    <button>Click</button>
+    <button children='click'/>
+  </div>
+)
+
+
+
+//ReactDON отвечает за то куда будет рендериться рект приложение в нашем приложении
+/*-------------18 version React-------------------*/
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  elem
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
+/*-------------17 version React-------------------*/
+// ReactDOM.render(
+// <React.StrictMode>
+// <App />
+// </React.StrictMode> , 
+// document.getElementById('root')
+// )
